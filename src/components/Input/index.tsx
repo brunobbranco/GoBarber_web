b55f1fffa6411d90759d57185e3314a050e80d7e
Bruno Benedet Branco <bruno.branco@FIDI.net>
2020-04-25 21:29:46 -0300

    complete

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,15 @@
-import React, { InputHTMLAttributes, ComponentType } from 'react';
-import { IconBaseProps } from 'react-icons';
+import React, { InputHTMLAttributes } from 'react';
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
+const Input: React.FC<InputProps> = (props) => (
   <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
+    <input {...props} />
   </Container>
-);
+)
 
 export default Input;
